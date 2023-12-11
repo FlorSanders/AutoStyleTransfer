bb395ad5d9c91643440fa48f81286196bead5b37
Flor Sanders <me@florsanders.be>
2023-12-10 20:41:04 -0500

    Final model training

--- tools/constants.py
+++ tools/constants.py
@@ -16,7 +16,7 @@ models_dir = "models"
 models_path = os.path.join(data_path, models_dir)
 
 # Preprocessing settings
-n_secs = 1
+n_secs = 3
 cut_classical_path = os.path.join(cut_classical_path, f"{n_secs}s")
 cut_jazz_path = os.path.join(cut_jazz_path, f"{n_secs}s")
 npy_classical_path = os.path.join(npy_classical_path, f"{n_secs}s")
@@ -25,5 +25,5 @@ npy_jazz_path = os.path.join(npy_jazz_path, f"{n_secs}s")
 ## Domain sizes
 default_sample_rate = 44100
 default_n_fft = 2048
-default_n_mels = 128
-default_n_mfcc = 128
+default_n_mels = 256
+default_n_mfcc = 256
